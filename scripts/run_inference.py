c57fc1363dc59e0f89ad58dc45a8b0e2ecbafb5c
pepepepepepo <Peace4342kato@gmail.com>
2025-09-18 21:48:00 +0900

    Update run_inference.py

--- scripts/run_inference.py
+++ scripts/run_inference.py
@@ -1,13 +1,14 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-MODEL_NAME = "facebook/opt-125m"  # 小さめで動作確認用 (CPUでも可)
+MODEL_NAME = "sshleifer/tiny-gpt2"  # 小さめで動作確認用
 
 tok = AutoTokenizer.from_pretrained(MODEL_NAME)
 model = AutoModelForCausalLM.from_pretrained(MODEL_NAME)
 
 prompt = "Hello from saijin-swallow!"
 inputs = tok(prompt, return_tensors="pt")
+
 out = model.generate(**inputs, max_new_tokens=40, do_sample=True, temperature=0.8)
 
-# ここを修正: .tolist() を付ける
+# ここがポイント: out[0] をリストにして decode
 print(tok.decode(out[0].tolist(), skip_special_tokens=True))
