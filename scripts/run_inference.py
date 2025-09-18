00c071138b440f08c17cb4830f3419b9ef86456b
pepepepepepo <Peace4342kato@gmail.com>
2025-09-18 21:37:10 +0900

    Update run_inference.py

--- scripts/run_inference.py
+++ scripts/run_inference.py
@@ -1,6 +1,6 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-MODEL_NAME = "facebook/opt-125m"  # 小さめで動作確認用（CPUでも可）
+MODEL_NAME = "facebook/opt-125m"  # 小さめで動作確認用 (CPUでも可)
 
 tok = AutoTokenizer.from_pretrained(MODEL_NAME)
 model = AutoModelForCausalLM.from_pretrained(MODEL_NAME)
@@ -8,4 +8,6 @@ model = AutoModelForCausalLM.from_pretrained(MODEL_NAME)
 prompt = "Hello from saijin-swallow!"
 inputs = tok(prompt, return_tensors="pt")
 out = model.generate(**inputs, max_new_tokens=40, do_sample=True, temperature=0.8)
-print(tok.decode(out[0], skip_special_tokens=True))
+
+# ここを修正: .tolist() を付ける
+print(tok.decode(out[0].tolist(), skip_special_tokens=True))
