dba971d4724f87d28ad77f9efd91d5b3b504ef95
pepepe <Peace4342kato@gmail.com>
2025-10-28 11:38:56 +0900

    Add navigation links to Layout component

---
+++ frontend/components/Layout.tsx
@@ -0,0 +1,4 @@
+<nav className="flex space-x-4">
+  <Link href="/" className="text-blue-600 hover:underline">ホーム</Link>
+  <Link href="/docs" className="text-blue-600 hover:underline">語温ガイド</Link>
+</nav>
